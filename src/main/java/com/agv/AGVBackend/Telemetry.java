69f1791512757f2fb24694459649aaa57a445a99
Hamzah Imran <hamzahimran102@gmail.com>
2025-05-27 01:25:18 -0400

    finished backend

--- src/main/java/com/agv/AGVBackend/Telemetry.java
+++ src/main/java/com/agv/AGVBackend/Telemetry.java
@@ -1,8 +1,11 @@
 package com.agv.AGVBackend;
+import lombok.Setter;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Setter
 public class Telemetry {
 
     @Id
@@ -17,23 +20,4 @@ public class Telemetry {
     private LocalDateTime timestamp = LocalDateTime.now();
 
     public Telemetry() {}
-
-    //Getters and Setters
-    public Long getId() { return id; }
-
-    public String getAgvId() { return agvId; }
-    public void setAgvId(String agvId) { this.agvId = agvId; }
-
-    public int getX() { return x; }
-    public void setX(int x) { this.x = x; }
-
-    public int getY() { return y; }
-    public void setY(int y) { this.y = y; }
-
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
-
-    public LocalDateTime getTimestamp() { return timestamp; }
-    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
-
 }
--- src/main/java/com/agv/AGVBackend/TelemetryRepository.java
+++ src/main/java/com/agv/AGVBackend/TelemetryRepository.java
@@ -3,6 +3,9 @@ package com.agv.AGVBackend;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public class TelemetryRepository {
-    //JPA handles basic DB operations, repository is an abstraction for database operations, has built-in methods
+@Repository
+public interface TelemetryRepository extends JpaRepository<Telemetry, Long> {
+    // The generic types are: <Entity type, ID type>
+    // No need to implement any methods - JPA will provide basic CRUD operations
 }
+
